f05d8ef73e858f367232b285fa703fbbc646f487
Shyam Kumar <shyamkumar@berkeley.edu>
2023-06-30 11:53:48 -0700

    Initial Commit

---
+++ R!GGED/R_GGEDApp.swift
@@ -0,0 +1,17 @@
+//
+//  R_GGEDApp.swift
+//  R!GGED
+//
+//  Created by Shyam Kumar on 6/30/23.
+//
+
+import SwiftUI
+
+@main
+struct R_GGEDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
